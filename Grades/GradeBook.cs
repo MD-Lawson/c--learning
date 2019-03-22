5d60de33109155e2aa62cc51ef10ff0b45df41c1
matthewl <matthew.lawson1@vodafone.com>
2019-03-22 14:19:55 +0000

    State of Code at end of Pluralsight Control Flow Module

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,48 @@ namespace Grades
 {
     public class GradeBook
     {
+        private List<float> grades;
+        private string _name;
+        public event NameChangedDelegate NameChanged;
 
-        private List<float> grades = new List<float>();
+        public GradeBook()
+        {
+            _name = "Empty";
+            grades = new List<float>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name Cannot be null or empty");
+                }
+
+                if (_name != value)
+                {
+                    NameChangeEventArgs args = new NameChangeEventArgs();
+                    args.ExistingName = _name;
+                    args.NewName = value;
+                    NameChanged(this, args);
+                }
+                _name = value;
+
+            }
+        }
+
+        public void WriteGrades(TextWriter destination)
+        {
+            for (int i = 0; i < grades.Count; i++)
+            {
+                destination.WriteLine(grades[i]);
+            }
+        }
 
         public void AddGrade(float grade)
         {
@@ -21,7 +62,7 @@ namespace Grades
             GradeStatistics stats = new GradeStatistics();
             float sum = 0;
             stats.HighestGrade = 0;
-            foreach(float grade in grades)
+            foreach (float grade in grades)
             {
                 stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
                 stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Speech.Synthesis;
 using System.Text;
@@ -11,20 +12,75 @@ namespace Grades
     {
         static void Main(string[] args)
         {
-            SpeechSynthesizer synth = new SpeechSynthesizer();
+            //SpeechSynthesizer synth = new SpeechSynthesizer();
 
-            synth.Speak("Hello! This is the grade book program");
+            //synth.Speak("Hello! This is the grade book program");
 
             GradeBook book = new GradeBook();
+
+            book.NameChanged += OnNameChanged;
+            GetBookName(book);
+            AddGrades(book);
+            SaveGrades(book);
+            WriteResults(book);
+        }
+
+        private static void WriteResults(GradeBook book)
+        {
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(book.Name);
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
+            WriteResult(stats.Description, stats.LetterGrade);
+        }
+
+        private static void SaveGrades(GradeBook book)
+        {
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+                book.WriteGrades(outputFile);
+            }
+        }
+
+        private static void AddGrades(GradeBook book)
+        {
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
+        }
 
-            GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(stats.AverageGrade);
-            Console.WriteLine(stats.HighestGrade);
-            Console.WriteLine(stats.LowestGrade);
+        private static void GetBookName(GradeBook book)
+        {
+            try
+            {
+                Console.WriteLine("Please enter a Name for the Grade Book: ");
+                book.Name = Console.ReadLine();
+            }
+            catch (ArgumentException err)
+            {
+                Console.WriteLine(err.Message);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Oops, Something went wrong.");
+                Console.WriteLine(err);
+            }
+        }
 
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine($"{description}: {result:F2}", description, result);
+        }
+        static void WriteResult(string description, string result)
+        {
+            Console.WriteLine($"{description}: {result:F2}", description, result);
+        }
+
+
+        static void OnNameChanged(object sender, NameChangeEventArgs args)
+        {
+            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}.");
         }
     }
 }
